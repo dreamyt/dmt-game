46c39b103d6a47e939978c4b50c59df4284617a8
xxz-python <906551562@qq.com>
2021-12-13 10:25:51 +0800

    Changes of Weapon Basis
    1. Scripts:
        CharComponent - CharWeapon
                                                |
                                        Weapon

---
+++ Assets/Scripts/Weapon/CharacterWeapon.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterWeapon : CharacterComponents
+{
+    [Header("Weapon Settings")]
+    [SerializeField] private Weapon weaponToUse;
+    [SerializeField] private Transform weaponHolderPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
