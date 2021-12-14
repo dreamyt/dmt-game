764b360b81f6212f4258715402b1fbfa7de6abe3
lilacwine <2846135079@qq.com>
2021-12-15 01:34:03 +0800

    new enemy

--- Assets/Scripts/AI/RoleDie.cs
+++ Assets/Scripts/AI/RoleDie.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class RoleDie : MonoBehaviour
 {
+    private Animator anim;
+
+    private Rigidbody2D rigid;
     //public GameObject prefabDeadFX;
     void Start()
     {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     public virtual void Die(Transform trans)
     {
         //Instantiate(prefabDeadFX, trans.position, Quaternion.identity);
-        Destroy(gameObject);
+        rigid.simulated = false;
+        anim.SetBool("Death", true);
+        Destroy(gameObject, 1.5f);
     }
 }
 
--- Assets/Scripts/AI/Type2.cs
+++ Assets/Scripts/AI/Type2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Type1 : MonoBehaviour
+public class Type2 : MonoBehaviour
 {
     Rigidbody2D rigid;
     Animator anim;
@@ -14,7 +14,12 @@ public class Type1 : MonoBehaviour
     [SerializeField] private float offsetX=0;
     [SerializeField] private float offsetY=0;
     [SerializeField] private float dist = 1;
-        
+    [SerializeField] private float detectRange = 1;
+    [SerializeField] private float attackRange = 1;
+    //animation control
+    private bool attacking=false;
+    private bool hurt=false;
+    private bool death=false;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -40,14 +45,30 @@ public class Type1 : MonoBehaviour
         bool forward_down = Physics2D.Raycast(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(face, -1), 1.3f*dist, LayerMask.GetMask("Tilemap_Platform"));
         bool forward = Physics2D.Raycast(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(face, 0), 1.2f*dist, LayerMask.GetMask("Tilemap_Platform"));
         bool down = Physics2D.Raycast(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(0, -1), 1f*dist, LayerMask.GetMask("Tilemap_Platform"));
+        bool player_behind = Physics2D.Raycast(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(-face, 0), detectRange*dist, LayerMask.GetMask("Player"));
+        bool attack_range = Physics2D.Raycast(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(face, 0), attackRange*dist, LayerMask.GetMask("Player"));
         DebugLine(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(face, -1), 1.3f*dist, forward_down);
         DebugLine(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(face, 0), 1.2f*dist, forward);
         DebugLine(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(0, -1), 1f*dist, down);
-
+        DebugLine(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(-face, 0), detectRange*dist, player_behind);
+        DebugLine(new Vector2(transform.position.x+offsetX, transform.position.y+offsetY), new Vector2(face, 0), attackRange*dist, attack_range);
         bool flip = false;
 
         if (down)
         {
+            if (player_behind)
+            {
+                flip = true;
+            }
+
+            if (attack_range)
+            {
+                attacking = true;
+            }
+            else
+            {
+                attacking = false;
+            }
             if (forward)
             {
                 flip = true;
@@ -64,5 +85,36 @@ public class Type1 : MonoBehaviour
         }
 
         rigid.velocity = new Vector2(face * speed, rigid.velocity.y);
+        SetAnim();
+    }
+
+    private void SetAnim()
+    {
+        if (death)
+        {
+            anim.SetBool("Attack", false);
+            anim.SetBool("Hurt", false);
+            anim.SetBool("Death", true);
+        }
+        else
+        {
+            if (hurt)
+            {
+                anim.SetBool("Attack", false);
+                anim.SetBool("Hurt", true);
+            }       
+            else
+            {
+                anim.SetBool("Hurt", false);
+                if (attacking)
+                {
+                    anim.SetBool("Attack", true);
+                }
+                else
+                {
+                    anim.SetBool("Attack", false);
+                }
+            }
+        }
     }
 }
